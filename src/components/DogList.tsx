623fa719e9396622a16c4c38e04ccea267ed146c
Bartek Maksimowski <maksimowski97@gmail.com>
2023-06-18 15:44:44 +0200

    app update

---
+++ src/components/DogList.tsx
@@ -0,0 +1,40 @@
+
+
+function DogList() {
+    
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        
+      };
+
+    return (
+        <div className="screen">
+            <div className="dog-list-container">
+                <input className="dog-list-input" type="text" placeholder="Wyszukaj Rasę Psa" onChange={onChange} />
+                <div className="dog-list">
+                    <ul>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                        <li>rot</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DogList;
---
+++ src/components/DogPhoto.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function DogPhoto() {
+    return (
+        <div className='screen'>
+            Witam
+        </div>
+    )
+}
+
+export default DogPhoto;
