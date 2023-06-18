c12db84b1ea9e910ac7b26d66a28997123030479
Bartek Maksimowski <maksimowski97@gmail.com>
2023-06-18 17:29:26 +0200

    app upgrade

--- src/components/DogPhoto.tsx
+++ src/components/DogPhoto.tsx
@@ -1,9 +1,20 @@
+import AnonymousDog from './resources/AnonymousDog.png';
 import React, { useState, useEffect } from 'react';
 
 function DogPhoto() {
+    const submit = () => {
+
+    }
+
     return (
         <div className='screen'>
-            Witam
+            <div className='dog-photo-container'>
+                <div className='dog-searcher'>
+                    <input type="text" placeholder='Wpisz Rasę' onSubmit={submit}/>
+                    <button>Szukaj</button>
+                </div>
+                <img src={AnonymousDog} alt="Choosen Dog"/>
+            </div>
         </div>
     )
 }
