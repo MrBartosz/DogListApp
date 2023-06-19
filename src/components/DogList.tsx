0bd002265793ae563f47a7fa33c0f7890ff59b12
Bartek Maksimowski <maksimowski97@gmail.com>
2023-06-19 10:13:20 +0200

    github pages fix

--- src/components/DogList.tsx
+++ src/components/DogList.tsx
@@ -1,45 +1,74 @@
-import React, { useState, useEffect  } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { fetchDogBreeds } from './DogApi';
-import { useNavigate  } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function DogList() {
-    const [dogBreeds, setDogBreeds] = useState<string[]>([]);
-    const [originalDogBreeds, setOriginalDogBreeds] = useState<string[]>([]);
-    const navigate  = useNavigate ();
+  const [dogBreeds, setDogBreeds] = useState<string[]>([]);
+  const [originalDogBreeds, setOriginalDogBreeds] = useState<string[]>([]);
+  const observerRef = useRef<HTMLDivElement | null>(null);
+  const loadedBreeds = useRef(15);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetchDogBreeds()
-        .then(breeds => {
-          setDogBreeds(breeds);
-          setOriginalDogBreeds(breeds);
-        });
-    }, []);
+  useEffect(() => {
+    fetchDogBreeds()
+    .then((breeds) => {
+      setOriginalDogBreeds(breeds);
+      setDogBreeds(breeds.slice(0, loadedBreeds.current));
+    });
+  }, []);
 
-    const handleClick = (breed: string) => {
-        navigate('/DogPhoto', { state: { breed } });
-    };
+  const loadMoreBreeds = () => {
+    const remainingBreeds = originalDogBreeds.slice(
+      loadedBreeds.current,
+      loadedBreeds.current + 10
+    );
+    setDogBreeds((prevBreeds) => [...prevBreeds, ...remainingBreeds]);
+    loadedBreeds.current += 10;
+  };
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const input = e.target.value.toLowerCase();
-        const filteredBreeds = originalDogBreeds.filter(breed => breed.toLowerCase().includes(input));
-        setDogBreeds(filteredBreeds);
-    };
+  const handleClick = (breed: string) => {
+    navigate('/DogPhoto', { state: { breed } });
+  };
 
-    return (
-        <div className="screen">
-            <div className="dog-list-container">
-                <input className="dog-list-input" type="text" placeholder="Wyszukaj Rasę Psa" onChange={onChange} />
-                <div className="dog-list">
-                    <ul>
-                        {dogBreeds.map(breed => (
-                            <li key={breed} onClick={() => handleClick(breed)}>{breed}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value.toLowerCase();
+    const filteredBreeds = originalDogBreeds.filter((breed) =>
+      breed.toLowerCase().includes(input)
+    );
+    setDogBreeds(filteredBreeds.slice(0, loadedBreeds.current));
+    loadedBreeds.current = loadedBreeds.current < 15 ? 15 : loadedBreeds.current;
+  };
+
+  const handleScroll = () => {
+    if (
+      observerRef.current &&
+      observerRef.current.scrollTop + observerRef.current.offsetHeight >= observerRef.current.scrollHeight - 1
+    ) {
+      loadMoreBreeds();
+    }
+  };
+
+  return (
+    <div className="screen">
+      <div className="dog-list-container">
+        <input
+          className="dog-list-input"
+          type="text"
+          placeholder="Wyszukaj Rasę Psa"
+          onChange={onChange}
+        />
+        <div className="dog-list" onScroll={handleScroll} ref={observerRef}>
+          <ul>
+            {dogBreeds.map((breed) => (
+              <li key={breed} onClick={() => handleClick(breed)}>
+                {breed}
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
 
 export default DogList;
